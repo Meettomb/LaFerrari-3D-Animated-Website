3018e3e67520518494fa410ec9380a072c6c71ea
Meettomb <patelmeet23032005@gmail.com>
2025-07-15 02:35:37 +0530

    Added Color Option in ThreeD View Section

--- Javascript/index2.js
+++ Javascript/index2.js
@@ -2,61 +2,67 @@ import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
-// let scene2, camera2, renderer2, hlight2, controls2, model;
 
-// function init2() {
-//   scene2 = new THREE.Scene();
 
-//   camera2 = new THREE.PerspectiveCamera(
-//     40,
-//     window.innerWidth / window.innerHeight,
-//     1,
-//     5000
-//   );
-//   camera2.position.set(0, 0.5, 3);
+let scene2, camera2, renderer2, hlight2, controls2, model;
 
-//   hlight2 = new THREE.AmbientLight(0x404040, 100);
-//   scene2.add(hlight2);
+function init2() {
+  scene2 = new THREE.Scene();
 
-//   const directPointLight = new THREE.DirectionalLight("white", 6);
-//   directPointLight.position.set(0, 1, 0);
-//   directPointLight.castShadow = true;
-//   scene2.add(directPointLight);
+  camera2 = new THREE.PerspectiveCamera(
+    40,
+    window.innerWidth / window.innerHeight,
+    1,
+    5000
+  );
+  camera2.position.set(0, 0.5, 3);
 
-//   renderer2 = new THREE.WebGLRenderer({
-//     canvas: document.querySelector(".wbgl2"),
-//     antialias: true,
-//     alpha: true,
-//   });
-//   renderer2.setSize(window.innerWidth, window.innerHeight);
-//   renderer2.setPixelRatio(window.devicePixelRatio);
+  hlight2 = new THREE.AmbientLight(0x404040, 100);
+  scene2.add(hlight2);
 
-//   // OrbitControls
-//   controls2 = new OrbitControls(camera2, renderer2.domElement);
-//   controls2.enableDamping = true;
-//   controls2.dampingFactor = 0.1;
-//   controls2.enableZoom = false;
+  const directPointLight = new THREE.DirectionalLight("white", 6);
+  directPointLight.position.set(0, 1, 0);
+  directPointLight.castShadow = true;
+  scene2.add(directPointLight);
 
-//   // Load Model
-//   const loader = new GLTFLoader();
-//   loader.load("Model/ferrari_laferrari_Black.glb", function (gltf) {
-//     model = gltf.scene;
-//     model.scale.set(50, 50, 50);
-//     model.position.set(0, -0.5, 0);
-//     model.rotation.x = -0.1;
-//     scene2.add(model);
-//   });
+  renderer2 = new THREE.WebGLRenderer({
+    canvas: document.querySelector(".wbgl2"),
+    antialias: true,
+    alpha: true,
+  });
+  renderer2.setSize(window.innerWidth, window.innerHeight);
+  renderer2.setPixelRatio(window.devicePixelRatio);
 
-//   animate();
-// }
+  // OrbitControls
+  controls2 = new OrbitControls(camera2, renderer2.domElement);
+  controls2.enableDamping = true;
+  controls2.dampingFactor = 0.1;
+  controls2.enableZoom = false;
 
-// function animate() {
-//   requestAnimationFrame(animate);
-//   if (model) {
-//     model.rotation.y += 0.005;
-//   }
-//   controls2.update();
-//   renderer2.render(scene2, camera2);
-// }
+  // Load Model
+  const loader = new GLTFLoader();
+  loader.load("Model/ferrari_laferrari_Black.glb", function (gltf) {
+    model = gltf.scene;
+    model.scale.set(50, 50, 50);
+    model.position.set(0, -0.5, 0);
+    model.rotation.x = -0.1;
+    scene2.add(model);
+  });
 
-// init2();
+  animate2();
+}
+
+function animate2() {
+  requestAnimationFrame(animate2);
+  if (model) {
+    model.rotation.y += 0.005;
+  }
+  controls2.update();
+  renderer2.render(scene2, camera2);
+}
+
+init2();
+
+
+
+
